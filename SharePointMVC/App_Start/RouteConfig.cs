2550b9f9cdee31781547ae3b1e2649081994052b
Bowab <Bowas92@gmail.com>
2018-10-23 20:29:26 +0200

    Login page added.

--- SharePointMVC/App_Start/RouteConfig.cs
+++ SharePointMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SharePointMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SharePoint", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SharePointMVC/Controllers/SharePointController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharePointMVC.Controllers
+{
+    public class SharePointController : Controller
+    {
+        // GET: SharePoint
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public void Login(string password, string email)
+        {
+            var x = password;
+        }
+    }
+}
